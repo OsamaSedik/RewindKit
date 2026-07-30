69631c8d860b47eb909a05d11d731b339dddc2ae
Osama S.Ahmed <42730159+OsamaSedik@users.noreply.github.com>
2026-07-31 01:28:04 +0300

    Add files via upload

---
+++ Source/RewindKit/Private/RewindTimeComponent.cpp
@@ -0,0 +1,141 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RewindTimeComponent.h"
+#include "Engine/World.h"        
+#include "TimerManager.h"        
+#include "GameFramework/Actor.h" 
+#include "DrawDebugHelpers.h"    
+
+// Sets default values for this component's properties
+URewindTimeComponent::URewindTimeComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+// Called when the game starts
+void URewindTimeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void URewindTimeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void URewindTimeComponent::StartRecordMovement()
+{
+	StopRewindMovement();
+	TimePhaseState = ETimePhaseState::ETP_Record;
+	OnTimePhaseChanged.Broadcast(TimePhaseState);
+	GetWorld()->GetTimerManager().SetTimer(RecordTimerHandle,this,&URewindTimeComponent::RecordMovement,0.016f,true);
+}
+
+void URewindTimeComponent::StopRecordMovement()
+{
+	if (GetWorld()->GetTimerManager().IsTimerActive(RecordTimerHandle))
+	{
+		GetWorld()->GetTimerManager().ClearTimer(RecordTimerHandle);
+	}
+}
+
+void URewindTimeComponent::RecordMovement()
+{
+	/** first check if the numbers of elements for the two arrays are grater than record time divided on the tick delta seconds */
+	const int32 Max_Positions_InTime = FMath::RoundToInt(RecordTime/ GetWorld()->GetDeltaSeconds());
+	const int32 Max_Rotations_InTime = FMath::RoundToInt(RecordTime/ GetWorld()->GetDeltaSeconds());
+	
+	if (RecordedPositions.Num()> Max_Positions_InTime && RecordedRotations.Num()>Max_Rotations_InTime )
+	{
+		/* remove the elements at last index to free up the data */
+		RecordedPositions.RemoveAt(RecordedPositions.Num() -1);
+		RecordedRotations.RemoveAt(RecordedRotations.Num() -1);
+	}
+	else
+	{
+		/* insert the owner location and rotation into the arrays */
+		RecordedPositions.Insert(GetOwner()->GetActorLocation(),0);
+		RecordedRotations.Insert(GetOwner()->GetActorRotation(),0);
+		DrawDebugRecord();
+	}
+}
+
+
+void URewindTimeComponent::StartRewindMovement()
+{
+	StopRecordMovement();
+	TimePhaseState = ETimePhaseState::ETP_Rewind;
+	OnTimePhaseChanged.Broadcast(TimePhaseState);
+	GetWorld()->GetTimerManager().SetTimer(RewindTimerHandle,this,&URewindTimeComponent::RewindMovement,0.016f,true);
+}
+
+void URewindTimeComponent::StopRewindMovement()
+{
+	if (GetWorld()->GetTimerManager().IsTimerActive(RewindTimerHandle))
+	{
+		GetWorld()->GetTimerManager().ClearTimer(RewindTimerHandle);
+	}
+}
+
+void URewindTimeComponent::RewindMovement()
+{
+	// if there are more data in the TArray for both Positions and Rotations
+	if (RecordedPositions.Num() > 0 && RecordedRotations.Num() > 0)
+	{
+		this->GetOwner()->SetActorLocation(RecordedPositions[0]);
+		this->GetOwner()->SetActorRotation(RecordedRotations[0]);
+
+		DrawDebugRecord();
+		/** remove the data that have been rewinded from the arrays */
+		RecordedPositions.RemoveAt(0);
+		RecordedRotations.RemoveAt(0);
+	}
+	else
+	{
+		//GEngine->AddOnScreenDebugMessage(-1,5.0f,FColor::Red,TEXT("Finished Rewind"));
+		GetWorld()->GetTimerManager().ClearTimer(RewindTimerHandle);
+	}
+}
+
+
+void URewindTimeComponent::DrawDebugRecord()
+{
+	if (!bShowDebugRecording || RecordedPositions.Num() < 2) return;
+
+	UWorld* World = GetWorld();
+	if (!World) return;
+
+	constexpr float LifeTime = 0.0f;
+	constexpr float AxisLength = 50.0f;
+	constexpr float ArrowSize = 15.0f;
+	constexpr float Thickness = 1.5f;
+
+	for (int32 i = 0; i < RecordedPositions.Num(); i += 5) // skip for performance
+	{
+		const FVector& Pos = RecordedPositions[i];
+		const FRotator& Rot = RecordedRotations[i];
+
+		// X-axis (Forward)
+		const FVector Forward = Rot.Vector();
+		DrawDebugDirectionalArrow(World,Pos,Pos + Forward * AxisLength,ArrowSize,
+			FColor::Red,false,LifeTime,0,Thickness);
+		
+		
+		// Y axis (Right)
+		const FVector Right = Rot.RotateVector(FVector::RightVector);
+		DrawDebugDirectionalArrow(World,Pos,Pos + Right * AxisLength,	ArrowSize
+			,FColor::Green,false,LifeTime,0,Thickness);
+		
+
+		// Z axis (Up)
+		const FVector Up = Rot.RotateVector(FVector::UpVector);
+		DrawDebugDirectionalArrow(World,Pos,Pos + Up * AxisLength,ArrowSize,
+			FColor::Blue,false,LifeTime,0,Thickness);
+	}
+}
+
+
+
+
---
+++ Source/RewindKit/Public/RewindTimeComponent.h
@@ -0,0 +1,87 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "RewindTimeComponent.generated.h"
+
+UENUM(BlueprintType)
+enum class ETimePhaseState : uint8
+{
+	ETP_Record		UMETA(DisplayName="Record"),
+	ETP_Rewind		UMETA(DisplayName="Rewind"),
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTimePhaseChanged,ETimePhaseState,TimePhaseState);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class REWINDKIT_API URewindTimeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	URewindTimeComponent();
+	
+	// show debug arrow for the three axis ( X, Y , Z )
+	UPROPERTY(BlueprintReadWrite,EditAnywhere,Category="Rewind Time")
+	bool bShowDebugRecording = false;
+	
+	// the state of time phase -> used for apply effects & anim blueprint 
+	UPROPERTY(BlueprintReadWrite,EditAnywhere,Category="Rewind Time")
+	ETimePhaseState TimePhaseState = ETimePhaseState::ETP_Record;
+	
+	// delegate callback for anim blueprint to inverse the animation or not 
+	UPROPERTY(BlueprintAssignable,Category="Rewind Time")
+	FOnTimePhaseChanged OnTimePhaseChanged;
+	
+	// time for recording set between 1 and 3 for performance 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Rewind Time", meta=(ClampMin="1.0", ClampMax="5.0", UIMin="1.0", UIMax="5.0"))
+	float RecordTime = 2.0f;
+	
+	// an array to store the actor Positions */
+	UPROPERTY(BlueprintReadOnly,VisibleAnywhere,Category="Rewind Time")
+	TArray<FVector> RecordedPositions;
+
+	// an array to store the actor Rotations */
+	UPROPERTY(BlueprintReadOnly,VisibleAnywhere,Category="Rewind Time")
+	TArray<FRotator> RecordedRotations;
+
+	// timers for record & rewind 
+	FTimerHandle RecordTimerHandle;
+	FTimerHandle RewindTimerHandle;
+	
+protected:
+	
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+	// draw debug axis for the actor 
+	UFUNCTION(BlueprintCallable,Category=" Rewind Time")
+	void DrawDebugRecord();
+	
+public:
+	// start recording and call RecordMovement() by a timer 
+	UFUNCTION(BlueprintCallable,Category=" Rewind Time")
+	void StartRecordMovement();
+	
+	/** a Function to record data into the positions and rotations */
+	UFUNCTION(BlueprintCallable,Category=" Rewind Time")
+	void RecordMovement();
+	
+	// stop the recording timer handle 
+	UFUNCTION(BlueprintCallable,Category=" Rewind Time")
+	void StopRecordMovement();
+	
+	// start rewinding and call RewindMovement() by a timer 
+	UFUNCTION(BlueprintCallable,Category=" Rewind Time")
+	void StartRewindMovement();
+	
+	/** a Function to reimport the data from the positions and rotations into the owner */
+	UFUNCTION(BlueprintCallable,Category=" Rewind Time")
+	void RewindMovement();
+	
+	// stop the rewind timer 
+	UFUNCTION(BlueprintCallable,Category=" Rewind Time")
+	void StopRewindMovement();
+};
